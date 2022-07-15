533c4abbdf9e3528495b5ffbae7403331c879784
DESKTOP-F892L4I\Tauhu <tbtoanst@gmail.com>
2022-07-15 23:46:38 +0700

    modify color in dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -5,7 +5,7 @@ function Dashboard() {
   return <>
     <div className="dashboard-container">
       <div className="content">
-        QUIZ() <span style={{ color: "#4bd1c6", marginLeft: "80px" }}>{"{"}</span>
+        QUIZ() <span style={{ color: "#29E3A8", marginLeft: "80px" }}>{"{"}</span>
       </div>
       <div className="content" style={{ paddingLeft: "100px" }}>
         Uptempo
@@ -14,7 +14,7 @@ function Dashboard() {
         dev test kit
       </div>
       <div className="content" style={{ paddingTop: "200px" }}>
-        <span style={{ color: "#4bd1c6", marginBottom: "20px" }}>{"}"}</span>
+        <span style={{ color: "#29E3A8", marginBottom: "20px" }}>{"}"}</span>
         <button className="start-btn">START QUIZ</button>
       </div>
     </div>
